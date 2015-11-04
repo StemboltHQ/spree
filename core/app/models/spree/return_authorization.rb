0246f5eaf79e0542efa270186bf059a855cbb126
Chris Todorov <chris@freerunningtechnologies.com>
2015-11-03 20:28:01 -0800

    Remove `dependent: nullify` for inventory units
    This change removes a `dependent: nullify` defined on the association to
    inventory units on Spree::ReturnAuthorization.
    
    This change is necessary because we are running with the
    ReturnAuthrization code from Solidus and that no longer has an
    association to inventory units, which causes this dependent callback to
    raise an error.
    
    Change-Id: Ic73861f7b975b5d1b0776df8a8775cbed0f5c165

--- core/app/models/spree/return_authorization.rb
+++ core/app/models/spree/return_authorization.rb
@@ -2,7 +2,7 @@ module Spree
   class ReturnAuthorization < ActiveRecord::Base
     belongs_to :order, class_name: 'Spree::Order', inverse_of: :return_authorizations
 
-    has_many :inventory_units, dependent: :nullify, inverse_of: :return_authorization
+    has_many :inventory_units, inverse_of: :return_authorization
     belongs_to :stock_location
     before_create :generate_number
     before_save :force_positive_amount
